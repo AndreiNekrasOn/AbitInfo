9f8f3246fb81508d0ba745089c497e794ddb3834
AndreiNekrasOn <an-32010@yandex.ru>
2018-07-24 19:05:45 +0300

    Settings changed for demo(debug=false)

--- abitinfo/settings.py
+++ abitinfo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '==m3p%5@@*-kp1^=h(+lwiqrlx(-ac&ml*3cj2a1a=xllufc73'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'andreinekrason.pythonanywhere.com']
 
 
 # Application definition
