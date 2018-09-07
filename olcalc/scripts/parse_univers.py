0ec607dc4a2818955f99f8f62eb7368500bfb2c5
Andrei Nekrasov <39444688+AndreiNekrasOn@users.noreply.github.com>
2018-09-08 00:49:34 +0300

    deleted writing into file part

--- olcalc/scripts/parse_univers.py
+++ olcalc/scripts/parse_univers.py
@@ -2,14 +2,6 @@ from olcalc.models import Univer_plus
 import requests
 from bs4 import BeautifulSoup
 
-url_base = "https://propostuplenie.ru/university/vuzy-moskvy/"
-f = open('html/uni.html', 'w', encoding='utf-8')
-for page_number in range(1, 19):
-    payload = {'Region[]': 15, 'UniverPager': page_number}
-    pag_code = requests.get(url_base, params=payload)
-    a = (str(pag_code.content.decode('utf-8')))
-    f.write(a)
-f.close()
 
 
 def run():
