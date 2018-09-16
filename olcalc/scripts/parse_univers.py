063f4e536dff0995ccd90dbb6255a798d36ffc9f
AndreiNekrasOn <an-32010@yandex.ru>
2018-09-16 19:26:47 +0300

    decodings, now should work

--- olcalc/scripts/parse_univers.py
+++ olcalc/scripts/parse_univers.py
@@ -4,27 +4,9 @@
 def run():
     page_code = open('uni.txt', encoding='utf-8')
     for line in page_code.readlines():
-        # Univer_plus.objects.create(title=line[0], s_title=line[1], passing_score=line[2])
-        cnt = 0
-        title = ''
-        s_title = ''
-        passing_scor = ''
         for chr in line:
-            if cnt == 0:
-                if chr == "'" or chr == "[":
-                    continue
-                else:
-                    if chr != "'":
-                        title += chr
-                    else:
-                        cnt = 1
-            if cnt == 1:
-                if chr == "'" or chr == "[":
-                    continue
-                else:
-                    if chr != "'":
-                        title += chr
-                    else:
-                        cnt = 1
+            if chr == '[' or chr == "'" or chr == ',':
+                chr = ''
+
 
 
--- olcalc/scripts/write_olymps.py
+++ olcalc/scripts/write_olymps.py
@@ -2,8 +2,8 @@ import requests
 
 url = "http://rsr-olymp.ru"
 page_code = requests.get(url)
-f = open('olymps_to_parse.txt', 'w')
-a = (str(page_code.content))
+f = open('olymps_to_parse.html', 'w')
+a = (str(page_code.content.decode('utf-8')))
 f.write(a)
 f.close()
 
