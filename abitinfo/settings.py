bf87dd00b5fe63f0dc5bf41e3b3027b1579e9311
AndreiNekrasOn <an-32010@yandex.ru>
2019-01-20 00:57:07 +0300

    now in request olymp_subject. it fixes problem with representation in result table

--- abitinfo/settings.py
+++ abitinfo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '==m3p%5@@*-kp1^=h(+lwiqrlx(-ac&ml*3cj2a1a=xllufc73'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'abitinfo.pythonanywhere.com']
 
--- olcalc/views.py
+++ olcalc/views.py
@@ -25,13 +25,16 @@ def project(request):
 
 def search(request):
     olymps = list(QOlymp.objects.order_by('title'))
-    if 'olymp_name' in request.GET and request.GET['olymp_name']:
+    if ('olymp_name' in request.GET and request.GET['olymp_name']) and \
+            ('olymp_subject' in request.GET and request.GET['olymp_subject']):
         olymp_name = request.GET['olymp_name']
-        qmap = QMapping.objects.filter(olymp__title=olymp_name)
+        olymp_subject = request.GET['olymp_subject']
+        qmap = QMapping.objects.all().filter(olymp__title=olymp_name).filter(olymp__subject=olymp_subject)
         set_qmap = set()
         for u in qmap:
             set_qmap.add(u)
-        content = {'olymp_name': olymp_name, 'qmap': set_qmap, 'olymps': olymps}
+        content = {'olymp_name': olymp_name, 'olymp_subject': olymp_subject,
+                   'qmap': set_qmap, 'olymps': olymps}
         return render_to_response('olcalc/index.html', content)
     else:
         return render(request, 'olcalc/index.html', {'olymps': olymps})
